cdc6978eaf71b11d43fa38efd00b4aa628f0e2b4
cutmvasya <meutia.vasya@gmail.com>
2021-11-04 23:04:30 +0700

    twice

--- 8 kyu/twiceAsOld.js
+++ 8 kyu/twiceAsOld.js
@@ -3,13 +3,34 @@
  * https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
  */
 
-function twiceAsOld(dadYearsOld, sonYearsOld) {
-    // your code here
-    let son = 2 * sonYearsOld
-    let pastDad = dadYearsOld - son
-    if (pastDad < 0) {
-        return pastDad * -1
-    }
-    return pastDad
+// function twiceAsOld(dadYearsOld, sonYearsOld) {
+//     // your code here
+//     let son = 2 * sonYearsOld
+//     let pastDad = dadYearsOld - son
+//     if (pastDad < 0) {
+//         return pastDad * -1
+//     }
+//     return pastDad
+// }
+// console.log(twiceAsOld(36, 7))
+
+// var a = ['dog', 'cat']
+// a[100] = 'fox'
+// console.log(a.length)
+
+// const foo = {
+//     bar() {
+//         console.log('hello')
+//     },
+//     name: 'albert'
+// }
+// console.log(foo)
+
+// const x = 6 % 2
+// const y = x ? 'One' : 'Two'
+// console.log(y)
+function logThis() {
+    this.desc = "log"
+    console.log(this)
 }
-console.log(twiceAsOld(36, 7))
+new logThis()
